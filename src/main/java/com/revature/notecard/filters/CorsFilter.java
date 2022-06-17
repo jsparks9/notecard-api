3c5eaadc81190b2271fa138e7bfe8ed8c0d026af
Josiah Sparks <josiah.sparks78@gmail.com>
2022-06-17 17:24:33 -0500

    added forwarding on filter

--- src/main/java/com/revature/notecard/filters/CorsFilter.java
+++ src/main/java/com/revature/notecard/filters/CorsFilter.java
@@ -1,25 +1,33 @@
 package com.revature.notecard.filters;
 
+import org.springframework.stereotype.Component;
+
 import javax.servlet.FilterChain;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebFilter
+@Component
 public class CorsFilter extends HttpFilter {
 
     public void doFilter(HttpServletRequest request, HttpServletResponse res, FilterChain chain){
-
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "*");
-        res.setHeader("Access-Control-Allow-Headers", "*");
-        /*
-        res.setHeader("Access-Control-Allow-Origin", "*"); //http://localhost:8080
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, " +
-                "Access-Control-Request-Method, Access-Control-Request-Headers");
-        res.setHeader("Access-Control-Allow-Credentials", "true");
-         */
-
+        try {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Methods", "*");
+            res.setHeader("Access-Control-Allow-Headers", "*");
+            /*
+            res.setHeader("Access-Control-Allow-Origin", "*"); //http://localhost:8080
+            res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+            res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, " +
+                    "Access-Control-Request-Method, Access-Control-Request-Headers");
+            res.setHeader("Access-Control-Allow-Credentials", "true");
+             */
+            chain.doFilter(request, res);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
 
     }
 }
