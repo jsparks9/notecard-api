9ba9acf295213a14b96607ada25237f1fa75eb9d
agent-kdubb <you@example.com>
2022-06-30 13:48:35 -0700

    Added comments explaining code

--- src/main/java/com/revature/notecard/receiving/RegisterController.java
+++ src/main/java/com/revature/notecard/receiving/RegisterController.java
@@ -13,15 +13,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class RegisterController {
 
+    //Give RegisterController access to UserRepository and UserService class
     private final UserRepository userRepo;
     private final UserService userService;
 
+
+    // Using @Autowired annotation to have Spring initialize the constructor with the UserRepository
+    // and UserService classes as parameters.
     @Autowired
     public RegisterController(UserRepository userRepo, UserService userService) {
         this.userRepo = userRepo;
         this.userService = userService;
     }
 
+
+    // Setting the Http response status to CREATED with @ResponseStatus
+    // Using @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(path="/register", consumes = "application/json", produces = "application/json")
     public ResponseEntity register(@RequestBody Register registerInfo) throws JsonProcessingException {
