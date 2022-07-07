8c33346a52ecad4782b540a5608986eb342c84d0
jsparks9 <66742639+jsparks9@users.noreply.github.com>
2022-07-07 15:25:05 -0500

    Create CreateDeck.java

---
+++ src/main/java/com/revature/notecard/service/dtos/CreateDeck.java
@@ -0,0 +1,13 @@
+package com.revature.notecard.service.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeckName {
+    private String deckname;
+}
